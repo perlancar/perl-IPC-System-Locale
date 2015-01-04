ea250b191343dca2a1a1d721282c85661a1ec14d
perlancar (@pc-home) <perlancar@gmail.com>
2015-01-04 11:05:42 +0700

    Add backtick()

--- lib/IPC/System/Locale.pm
+++ lib/IPC/System/Locale.pm
@@ -10,7 +10,7 @@ use warnings;
 use IPC::System::Options ();
 
 use Exporter qw(import);
-our @EXPORT_OK = qw(system);
+our @EXPORT_OK = qw(system backtick);
 
 sub system {
     my $opts = ref($_[0]) eq 'HASH' ? shift : {};
@@ -21,18 +21,30 @@ sub system {
     }, @_);
 }
 
+sub backtick {
+    my $opts = ref($_[0]) eq 'HASH' ? shift : {};
+
+    IPC::System::Options::backtick({
+        lang => 'C', # default
+        %$opts,
+    }, @_);
+}
+
 1;
-# ABSTRACT: Set language before executing system()
+# ABSTRACT: Set language before executing system() or backtick/qx
 
 =head1 SYNOPSIS
 
- use IPC::System::Locale qw(system);
+ use IPC::System::Locale qw(system backtick);
 
  # set locale to C
  system(...);
 
  # set locale explicitly
  system({lang=>"id_ID"} ...);
+
+ # same for backtick (qx)
+ my $output = backtick("df");
 
 
 =head1 DESCRIPTION
@@ -43,6 +55,14 @@ sub system {
 
 This is a shortcut for C<<IPC::System::Options::system({lang=>"C"}, ...)>> (you
 can also set C<lang> or other options explicitly).
+
+This routine might be helpful if you are checking the output of some program and
+want to make sure that the output is in English/C locale.
+
+=head2 backtick([ \%opts ], @args)
+
+This is a shortcut for C<<IPC::System::Options::backtick({lang=>"C"}, ...)>>
+(you can also set C<lang> or other options explicitly).
 
 This routine might be helpful if you are checking the output of some program and
 want to make sure that the output is in English/C locale.
